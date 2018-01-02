accfa7d9adfd4ce6d3e4cfeb9a901119c249c6e8
s147300 <s147300@student.dtu.dk>
2018-01-02 14:58:08 +0100

    test

---
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/trololo.java
@@ -0,0 +1,13 @@
+package com.example.jonathanlarsen.pensionconsultmainpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class trololo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trololo);
+    }
+}
