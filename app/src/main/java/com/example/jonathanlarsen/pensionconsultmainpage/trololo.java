a6b794bf60e8ca7bfed34c7de488a0327069828c
Danny <dannyjoensson@gmail.com>
2018-01-02 15:02:02 +0100

    testing github

--- app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/trololo.java
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/trololo.java
@@ -9,5 +9,6 @@ public class trololo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trololo);
+        // test
     }
 }
