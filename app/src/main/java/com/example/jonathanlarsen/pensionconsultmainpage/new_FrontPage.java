97ae4ae8d0de39cc18573be09dba40bae1916077
s147300 <s147300@student.dtu.dk>
2018-01-03 15:24:16 +0100

    New FP example

---
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/new_FrontPage.java
@@ -0,0 +1,13 @@
+package com.example.jonathanlarsen.pensionconsultmainpage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class new_FrontPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new__front_page);
+    }
+}
