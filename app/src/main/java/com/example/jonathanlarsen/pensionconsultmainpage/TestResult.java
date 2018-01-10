aef410d38c5275e889ed00438aaa0cb106f56f20
s147300 <s147300@student.dtu.dk>
2018-01-10 11:52:41 +0100

    TestQuestion class revisited

--- app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/TestResult.java
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/TestResult.java
@@ -56,7 +56,7 @@ public class TestResult extends AppCompatActivity implements View.OnClickListene
         }
 
         else if (view== endTest){
-            Intent i = new Intent(this, FrontPage.class);
+            Intent i = new Intent(this, new_FrontPage.class);
             startActivity(i);
             finish();
         }
