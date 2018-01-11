390947efba1f2e56ac664d51256e472f1a723191
s147300 <s147300@student.dtu.dk>
2018-01-11 11:50:16 +0100

    test-result page test and layout changes

--- app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/TestResult.java
+++ app/src/main/java/com/example/jonathanlarsen/pensionconsultmainpage/TestResult.java
@@ -30,19 +30,19 @@ public class TestResult extends AppCompatActivity implements View.OnClickListene
 
         if (value < 2.84) {
             headline.setText("Risikobetonet invisteringsprofil");
-            resultText.setText("Du er villig til yadayayada");
+            resultText.setText("Med en risikobetonet invisteringsprofil er du villig til at tage chancer for muligheden for større afkast. Vi anbefaler det til yngre personer, der kan nå at geninvinde eventuelle tab, samt til personer som tror de vil være mindre afhængige af deres pension som ældre.");
         }
         if (value > 2.84 && value < 3.15) {
             headline.setText("Gennemsnitlig invisteringsprofil");
-            resultText.setText("Du er villig til yadayayada");
+            resultText.setText("Med en gennemsnitlig invisteringsprofil er du villig til at tage chancer, men måske kun i dele af din opsparingsperiode. Det kan f.eks. være givligt for dig invisterer din pension i aktier imens du er ung og senere skifte over til obligationer");
         }
         if (value > 3.15 && value < 3.48) {
             headline.setText("Forsigtig invisteringsprofil");
-            resultText.setText("Du er villig til yadayayada");
+            resultText.setText("Med en forsigtig invisteringsprofil er det bedste at holde sig til invistering med lave svingninger, typisk obligatione. Der kan dog stadig være fordele ved at ændre til en mere risikobetonet gruppe i perioder af dit liv. Vi vil gerne hjælpe dig med at tage beslutninger omkring din pension undervejs. ");
         }
         if (value > 3.48) {
             headline.setText("Meget lav invisteringsprofil");
-            resultText.setText("Du er villig til yadayayada");
+            resultText.setText("Med en meget lav invisteringsprofil sværger du til de mest sikre invisteringer. Her er du sikker på at din pension ikke kommer ud for større tab på bekostning af invisteringer \n på den måde er din mulige gevinst også lille, men det du indbetaler er det du kan regne med står tilbage når tiden kommer.");
         }
 
     }
